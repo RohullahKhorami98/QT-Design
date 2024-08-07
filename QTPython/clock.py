018da5f1466c5887c111b48d6de89ccd345c2170
rohullah <rohullah.khorami98@gmail.com>
2024-08-07 12:02:26 +0200

    Created the watch

--- QTPython/clock.py
+++ QTPython/clock.py
@@ -1,32 +1,36 @@
 import sys
 from PyQt5 import QtWidgets, uic, QtCore
+from datetime import datetime
+
 class MyButtons(QtWidgets.QMainWindow):
     def __init__(self):
         super(MyButtons, self).__init__()
-        uic.loadUi('buttons.ui', self)
+        uic.loadUi('clock.ui', self)
         self.setupUI()
 
     def setupUI(self):
-        self.pushButton.clicked.connect(self.on_click)
-        self.clearButton.clicked.connect(self.on_clear)
+        self.pushButton.clicked.connect(self.start_timer)
         self.exitButton.clicked.connect(self.on_close)
+        self.timer = QtCore.QTimer(self)
+        self.timer.timeout.connect(self.update_time)
 
-    def on_click(self):
-        self.label.setText("Button was clicked!")
+    def start_timer(self):
+        self.timer.start(1000)  
 
-    def on_clear(self):
-        self.label.setText("")
-    
+    def update_time(self):
+        times = datetime.now()
+        convertedTime = times.strftime("%d-%m-%Y %H:%M:%S")
+        self.label.setText(str(convertedTime))
+
     def on_close(self):
         reply = QtWidgets.QMessageBox.question(self, 'Message',
                                                "Are you sure to quit?",
                                                QtWidgets.QMessageBox.Yes,
-                                               QtWidgets.QMessageBox.No) 
-        if reply == QtWidgets.QMessageBox.Yes: 
+                                               QtWidgets.QMessageBox.No)
+        if reply == QtWidgets.QMessageBox.Yes:
             self.close()
-        else: 
+        else:
             pass
-    
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
